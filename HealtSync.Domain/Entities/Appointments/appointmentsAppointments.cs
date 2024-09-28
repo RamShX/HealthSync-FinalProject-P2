1f373339ff785951f887e29a0d64c422ae868c81
Ramshley Polanco V. <ramshleyv@gmail.com>
2024-09-28 19:04:58 -0400

    Se agrego el modulo Appointments a la capa de Domain

---
+++ HealtSync.Domain/Entities/Appointments/appointmentsAppointments.cs
@@ -0,0 +1,14 @@
+﻿using HealtSync.Domain.Base;
+
+namespace HealtSync.Domain.Entities.Appointments
+{
+    public sealed class appointmentsAppointments : AuditableEntity
+    {
+        public int AppointmentID { get; set; }
+        public int PacientID { get; set; }
+        public int DoctorID { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public int StatusID { get; set; }
+      
+    }
+}
