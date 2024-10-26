d3256c96beb1d95c7ec6ede3e4ce8716cd32f666
Yeury_dcm <yeury.dcm2406@gmail.com>
2024-10-26 13:09:36 -0400

    Resolución conflictos Assembly

--- HealtSync.Persistence/obj/Debug/net8.0/HealtSync.Persistence.AssemblyInfo.cs
+++ HealtSync.Persistence/obj/Debug/net8.0/HealtSync.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealtSync.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7915ff8a23281935cfdc76c02366c8774184c086")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c15ac80cfc93fd0c4e797bd7d4ffe6d654c3552")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealtSync.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealtSync.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
